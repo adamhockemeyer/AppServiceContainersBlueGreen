fe81376dfd4fc0163b9e86a4bbc0955ad2792b9e
Adam Hockemeyer <hock2k5@gmail.com>
2021-10-18 13:48:23 -0400

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AppServiceContainersBlueGreen.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace AppServiceContainersBlueGreen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["BuildVersion"] = _configuration.GetValue<string>("BuildVersion");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
